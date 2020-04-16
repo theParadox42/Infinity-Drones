6579e2bacb41d7d2bd6ace8c726a81e1fbdfa994
theParadox42 <nathaniel.fargo@gmail.com>
2020-04-16 15:03:07 -0600

    Some work on custom physics

--- Infinity Drones/Assets/Scripts/PhysicsBody.cs
+++ Infinity Drones/Assets/Scripts/PhysicsBody.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class PhysicsBody : MonoBehaviour
 {
-    [SerializedField] float gravityModifyer = 1f;
 
+    [SerializeField] float minGroundNormalY = .65f;
+    [SerializeField] float gravityModifyer = 1f;
+
+    protected Vector2 targetVelocity;
+    protected bool grounded;
+    protected Vector2 groundNormal;
     protected Rigidbody2D rb;
     protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D>hitBufferList = new List<RaycastHit2D>(16);
+
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
 
     void OnEnable() {
         rb = GetComponent<Rigidbody2D>();
@@ -16,7 +28,9 @@ public class PhysicsBody : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask (gameObject.layer));
+        contactFilter.useLayerMask = true;
     }
 
     // Update is called once per frame
@@ -27,15 +41,55 @@ public class PhysicsBody : MonoBehaviour
 
     void FixedUpdate() {
         velocity += gravityModifyer * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+
+        grounded = false;
         
         Vector2 deltaPosition = velocity * Time.deltaTime;
 
-        Vector2 move = Vector2.up * deltaPosition.y;
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
 
-        Movement (move);
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement (move, false);
+
+        move = Vector2.up * deltaPosition.y;
+
+        Movement (move, true);
     }
 
-    void Movement(Vector2 move) {
-        rb.position = rb.position + move;
+    void Movement(Vector2 move, bool yMovement) {
+        float distance = move.magnitude;
+        
+        if (distance > minMoveDistance) {
+
+            int count = rb.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear ();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer [i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList [i].normal;
+                if (currentNormal.y > minGroundNormalY) {
+                    grounded = true;
+                    if (yMovement) {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0) {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        rb.position = rb.position + move.normalized * distance;
     }
 }
---
+++ Infinity Drones/Assets/Scripts/Proner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proner : PhysicsBody
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetVelocity = Vector2.left;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
