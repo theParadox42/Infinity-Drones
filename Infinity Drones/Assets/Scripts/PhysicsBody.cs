aabf1f1c1ee570e2075a4069a2c6c6fa1781bc7d
theParadox42 <nathaniel.fargo@gmail.com>
2020-04-16 14:25:49 -0600

    Did some basic stuff

---
+++ Infinity Drones/Assets/Scripts/PhysicsBody.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsBody : MonoBehaviour
+{
+    [SerializedField] float gravityModifyer = 1f;
+
+    protected Rigidbody2D rb;
+    protected Vector2 velocity;
+
+    void OnEnable() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        velocity += gravityModifyer * Physics2D.gravity * Time.deltaTime;
+        
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+
+        Movement (move);
+    }
+
+    void Movement(Vector2 move) {
+        rb.position = rb.position + move;
+    }
+}
