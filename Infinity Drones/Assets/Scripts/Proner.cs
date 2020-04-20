18836826304ee66bdea0171882f8f9c8f9572175
theParadox42 <nathaniel.fargo@gmail.com>
2020-04-20 15:00:23 -0600

    Yeah so better drone movement

--- Infinity Drones/Assets/Scripts/Proner.cs
+++ Infinity Drones/Assets/Scripts/Proner.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Proner : MonoBehaviour
 {
 
-    [SerializeField] private PlayerController player;
-    [SerializeField] private float droneSpeed = 3f;
+    [SerializeField] private PlayerController player = null;
+    [SerializeField] private float droneAcceleration = 2f;
+    [SerializeField] private float droneTargetSpeed = 8f;
     [SerializeField] private float targetDistance = 4f;
 
     private Vector2 playerDisplacement;
-    private Vector2 velocity;
+    // private Vector2 velocity;
 
     private Rigidbody2D rb;
 
@@ -24,13 +25,17 @@ public class Proner : MonoBehaviour
     void Update()
     {
         playerDisplacement = player.transform.position - transform.position;
-        
-        velocity = playerDisplacement.normalized * droneSpeed * GetSpeedModifyer();
-        rb.velocity = velocity;
+        var modifyer = GetSpeedModifyer(playerDisplacement.magnitude - targetDistance);
+        rb.velocity += playerDisplacement.normalized * droneAcceleration * modifyer;
+        if (rb.velocity.magnitude > droneTargetSpeed) {
+            rb.velocity *= 0.5f;
+        } else {
+            rb.velocity *= 0.98f;
+        }
     }
 
-    float GetSpeedModifyer () {
-        float n = Mathf.Pow(2, playerDisplacement.magnitude - targetDistance);
+    float GetSpeedModifyer (float distance) {
+        float n = Mathf.Pow(2, distance);
         return 2f * n / (n + 1f) - 1f;
     }
     
